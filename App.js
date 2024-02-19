fcdf4497068ed591d5aad5624a16d70b1c2c58de
Daniel Gruitt <danielgruitt@Daniels-MacBook-Pro.local>
2024-02-19 11:25:43 +0000

    updating husky

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
+import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { PaperProvider } from 'react-native-paper';
 
 export default function App() {
   return (
+
+    <NavigationContainer>
+      <PaperProvider>
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
+    </PaperProvider>
+    </NavigationContainer>
   );
 }
 
