82a354a61fff31fa01c443995dea2e232a7b4380
Daniel Gruitt <danielgruitt@Daniels-MBP.wifi.quayside.net.generatorhub.co.uk>
2024-02-20 13:49:47 +0000

    added footer to new user page

---
+++ pages/UserAccountPage.js
@@ -0,0 +1,15 @@
+import { View, Text } from "react-native";
+import FooterNav from "../components/FooterNav";
+
+const UserAccountPage = () => {
+  return (
+    <>
+      <View>
+        <Text>This is the User page</Text>
+      </View>
+      <FooterNav />
+    </>
+  );
+};
+
+export default UserAccountPage;
---
+++ pages/UserSettings.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const UserSettings = () => {
+    return ( 
+        <View>
+            <Text>This is the User Settings</Text>
+        </View>
+     );
+}
+ 
+export default UserSettings;
