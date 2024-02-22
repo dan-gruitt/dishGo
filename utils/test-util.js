09582312a30895d3516910f7b46fe2bed219f3fc
Daniel Gruitt <danielgruitt@Daniels-MBP.wifi.quayside.net.generatorhub.co.uk>
2024-02-22 09:59:34 +0000

    added util test

---
+++ utils/test-util.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function testutil() {
+  return (
+    <View>
+      <Text>test-util</Text>
+    </View>
+  )
+}
