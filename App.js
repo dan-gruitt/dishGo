1a4d292bd6679ec8f58526f33562a8278a4d5762
daniel gruitt <dan@gruitt.com>
2024-02-24 23:34:13 +0000

    added an FAQ page with palceholder buttons and text ready for styling

--- App.js
+++ App.js
@@ -10,62 +10,67 @@ import ResultsPage from "./pages/ResultsPage";
 import SearchPage from "./pages/SearchPage";
 import RestaurantPage from "./pages/RestaurantPage";
 import BusinessSignUp from "./pages/BusinessSignUp";
-import { UserProvider } from './context/UserContext';
+import { UserProvider } from "./context/UserContext";
 import TestPage from "./pages/TestPage";
 import BusinessMenuPage from "./pages/BusinessMenuPage";
-const Stack = createNativeStackNavigator()
+import Faq from "./pages/Faq";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-
   return (
-
-<UserProvider>
-    <PaperProvider>
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen
-            name="LandingPage"
-            component={LandingPage}
-            options={{ title: "Landing Page" }}
-          />
-          <Stack.Screen
-            name="AddRestaurantPage"
-            component={AddRestaurantPage}
-            options={{ title: "Add restaurant page" }}
-          />
-          <Stack.Screen
-            name="BusinessMenuPage"
-            component={BusinessMenuPage}
-            options={{ title: "Add menu page" }}
-          />
-          <Stack.Screen
-            name="ResultsPage"
-            component={ResultsPage}
-            options={{ title: "Result Page" }}
-          />
-          <Stack.Screen
-            name="RestaurantPage"
-            component={RestaurantPage}
-            options={{ title: "RestaurantPage" }}
-          />
-          <Stack.Screen
-            name="SearchPage"
-            component={SearchPage}
-            options={{ title: "Search Page" }}
-          />
-          <Stack.Screen
-            name="BusinessSignUp"
-            component={BusinessSignUp}
-            options={{ title: "Business SignUp" }}
-          />
-          <Stack.Screen
-            name="TestPage"
-            component={TestPage}
-            options={{ title: "Test Page" }}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </PaperProvider>
+    <UserProvider>
+      <PaperProvider>
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen
+              name="LandingPage"
+              component={LandingPage}
+              options={{ title: "Landing Page" }}
+            />
+            <Stack.Screen
+              name="AddRestaurantPage"
+              component={AddRestaurantPage}
+              options={{ title: "Add restaurant page" }}
+            />
+            <Stack.Screen
+              name="BusinessMenuPage"
+              component={BusinessMenuPage}
+              options={{ title: "Add menu page" }}
+            />
+            <Stack.Screen
+              name="ResultsPage"
+              component={ResultsPage}
+              options={{ title: "Result Page" }}
+            />
+            <Stack.Screen
+              name="RestaurantPage"
+              component={RestaurantPage}
+              options={{ title: "RestaurantPage" }}
+            />
+            <Stack.Screen
+              name="SearchPage"
+              component={SearchPage}
+              options={{ title: "Search Page" }}
+            />
+            <Stack.Screen
+              name="BusinessSignUp"
+              component={BusinessSignUp}
+              options={{ title: "Business SignUp" }}
+            />
+            <Stack.Screen
+              name="TestPage"
+              component={TestPage}
+              options={{ title: "Test Page" }}
+            />
+            <Stack.Screen
+              name="Faq"
+              component={Faq}
+              options={{ title: "Faq" }}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </PaperProvider>
     </UserProvider>
   );
 }
