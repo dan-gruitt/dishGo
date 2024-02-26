215779db47b0d13c7126b8252e5ad44b32678292
simmi <22905246+noepse@users.noreply.github.com>
2024-02-26 20:46:08 +0000

    remove comments

--- component/ImagePreview.tsx
+++ component/ImagePreview.tsx
@@ -1,7 +1,5 @@
 import { FileObject } from '@supabase/storage-js';
 import { Image, View, Text, TouchableOpacity } from 'react-native';
-import { supabase } from '../lib/supabase';
-import { useState } from 'react';
 import { Ionicons } from '@expo/vector-icons';
 
 // Image item component that displays the image from Supabase Storage and a delte button
