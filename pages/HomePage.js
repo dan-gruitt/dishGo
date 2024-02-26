e441831130c85c50c8b27f55267c687a31af670b
ClaudioCamba <33432769+ClaudioCamba@users.noreply.github.com>
2024-02-26 17:36:10 +0000

    styled bottom nav

--- pages/HomePage.js
+++ pages/HomePage.js
@@ -1,18 +1,80 @@
-import React from 'react'
+import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import SearchStack from '../navstack/SearchStack';
 import FavouriteStack from '../navstack/FavouriteStack';
 import SettingStack from '../navstack/SettingStack';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import { Text } from 'react-native';
 
 const Tab = createBottomTabNavigator();
 
 export default function HomePage() {
- 
+  const green = '#3AD6A7';
+  const grey = '#4C5B61';
+  const tabBarHeight = 70; 
+
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Search"  options={{ headerShown: false }} component={SearchStack}/>
-      <Tab.Screen name="Favourites" component={FavouriteStack} />
-      <Tab.Screen name="Settings" component={SettingStack} />
+    <Tab.Navigator
+      screenOptions={{
+        tabBarActiveTintColor: green, 
+        tabBarInactiveTintColor: grey,
+        tabBarStyle: {
+          paddingTop: 6,
+          paddingBottom: 12,
+          height: tabBarHeight,
+        }, 
+      }}>
+      <Tab.Screen 
+        name="Search"  
+        options={{
+          headerShown: false,
+          tabBarLabel: ({ color }) => (
+            <Text style={{ color }}>Search</Text>
+          ),
+          tabBarIcon: ({ color, size, focused }) => (
+            <MaterialCommunityIcons 
+              name="magnify" 
+              color={focused ? green : grey} 
+              size={size} 
+            />
+          ),
+        }} 
+        component={SearchStack}
+      />
+      <Tab.Screen 
+        name="Favourites" 
+        options={{
+          headerShown: false,
+          tabBarLabel: ({ color }) => (
+            <Text style={{ color }}>Favourites</Text>
+          ),
+          tabBarIcon: ({ color, size, focused }) => (
+            <MaterialCommunityIcons 
+              name="heart" 
+              color={focused ? green : grey} 
+              size={size} 
+            />
+          ),
+        }}
+        component={FavouriteStack} 
+      />
+      <Tab.Screen 
+        name="Settings" 
+        options={{
+          headerShown: false,
+          tabBarLabel: ({ color }) => (
+            <Text style={{ color }}>Settings</Text>
+          ),
+          tabBarIcon: ({ color, size, focused }) => (
+            <MaterialCommunityIcons 
+              name="cog" 
+              color={focused ? green : grey} 
+              size={size} 
+            />
+          ),
+        }} 
+        component={SettingStack} 
+      />
     </Tab.Navigator>
-  )
+  );
 }
--- pages/HomePageBusiness.js
+++ pages/HomePageBusiness.js
@@ -1,19 +1,62 @@
 import React from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import SettingStack from '../navstack/SettingStack';
 import UserBusinessProfile from '../component/UserBusinessProfile'
 import BusinessManagerStack from '../navstack/BusinessManagerStack';
-import BusinessMenuPage from './BusinessMenuPage';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import { Text } from 'react-native';
 
 const Tab = createBottomTabNavigator();
 
 export default function HomePageBusiness() {
- 
+  const green = '#3AD6A7';
+  const grey = '#4C5B61';
+  const tabBarHeight = 70; 
+
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Profile" options={{ headerShown: false }} component={UserBusinessProfile}/>
-      <Tab.Screen name="Locations" options={{ headerShown: false }} component={BusinessManagerStack} />
-      {/* <Tab.Screen name="Menus" component={BusinessMenuPage} /> */}
+    <Tab.Navigator
+      screenOptions={{
+        tabBarActiveTintColor: green, 
+        tabBarInactiveTintColor: grey,
+        tabBarStyle: {
+          paddingTop: 6,
+          paddingBottom: 12,
+          height: tabBarHeight,
+        }, 
+      }}>
+      <Tab.Screen 
+        name="Profile"  
+        options={{
+          headerShown: false,
+          tabBarLabel: ({ color }) => (
+            <Text style={{ color }}>Profile</Text>
+          ),
+          tabBarIcon: ({ color, size, focused }) => (
+            <MaterialCommunityIcons 
+              name="magnify" 
+              color={focused ? green : grey} 
+              size={size} 
+            />
+          ),
+        }} 
+        component={UserBusinessProfile}
+      />
+      <Tab.Screen 
+        name="Locations" 
+        options={{
+          headerShown: false,
+          tabBarLabel: ({ color }) => (
+            <Text style={{ color }}>Locations</Text>
+          ),
+          tabBarIcon: ({ color, size, focused }) => (
+            <MaterialCommunityIcons 
+              name="garage-variant" 
+              color={focused ? green : grey} 
+              size={size} 
+            />
+          ),
+        }}
+        component={BusinessManagerStack} 
+      />
     </Tab.Navigator>
-  )
+  );
 }
