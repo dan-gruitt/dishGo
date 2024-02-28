b07d75408f84665bae378624a2f926a0f3f768eb
daniel gruitt <dan@gruitt.com>
2024-02-28 17:14:31 +0000

    styled profiles but few issues with inheritence

--- component/Account.tsx
+++ component/Account.tsx
@@ -1,49 +1,47 @@
-import { useState, useEffect, useContext } from 'react'
-import { supabase } from '../lib/supabase'
-import { StyleSheet, View, Alert } from 'react-native'
-import { Button, Input } from 'react-native-elements'
-import { Session } from '@supabase/supabase-js'
-import Avatar from './Avatar'
-import { UserContext } from '../context/UserContext'
+import { useState, useEffect, useContext } from "react";
+import { supabase } from "../lib/supabase";
+import { StyleSheet, View, Alert, Pressable, Text } from "react-native";
+import { Button, Input } from "react-native-elements";
+import { Session } from "@supabase/supabase-js";
+import Avatar from "./Avatar";
+import { UserContext } from "../context/UserContext";
 
 export default function Account({ session }: { session: Session }) {
-  const [loading, setLoading] = useState(true)
-  const [username, setUsername] = useState('')
-  const [website, setWebsite] = useState('')
-  const [avatarUrl, setAvatarUrl] = useState('')
-  const {User, setUser} = useContext(UserContext)
-
+  const [loading, setLoading] = useState(true);
+  const [username, setUsername] = useState("");
+  const [website, setWebsite] = useState("");
+  const [avatarUrl, setAvatarUrl] = useState("");
+  const { User, setUser } = useContext(UserContext);
 
   useEffect(() => {
-    if (session) getProfile()
-  }, [session])
+    if (session) getProfile();
+  }, [session]);
 
   async function getProfile() {
     try {
-      setLoading(true)
-      if (!session?.user) throw new Error('No user on the session!')
+      setLoading(true);
+      if (!session?.user) throw new Error("No user on the session!");
 
       const { data, error, status } = await supabase
-        .from('profiles')
+        .from("profiles")
         .select(`*`)
-        .eq('id', session?.user.id)
-        .single()
+        .eq("id", session?.user.id)
+        .single();
       if (error && status !== 406) {
-        throw error
+        throw error;
       }
 
       if (data) {
-        setUsername(data.username)
-        setWebsite(data.website)
-        setAvatarUrl(data.avatar_url)
+        setUsername(data.username);
+        setWebsite(data.website);
+        setAvatarUrl(data.avatar_url);
       }
-
     } catch (error) {
       if (error instanceof Error) {
-        Alert.alert(error.message)
+        Alert.alert(error.message);
       }
     } finally {
-      setLoading(false)
+      setLoading(false);
     }
   }
 
@@ -52,13 +50,13 @@ export default function Account({ session }: { session: Session }) {
     website,
     avatar_url,
   }: {
-    username: string
-    website: string
-    avatar_url: string
+    username: string;
+    website: string;
+    avatar_url: string;
   }) {
     try {
-      setLoading(true)
-      if (!session?.user) throw new Error('No user on the session!')
+      setLoading(true);
+      if (!session?.user) throw new Error("No user on the session!");
 
       const updates = {
         id: session?.user.id,
@@ -66,85 +64,146 @@ export default function Account({ session }: { session: Session }) {
         website,
         avatar_url,
         updated_at: new Date(),
-      }
+      };
 
-      const { data, error } = await supabase.from('profiles')
-      .upsert(updates)
-      .select()
+      const { data, error } = await supabase
+        .from("profiles")
+        .upsert(updates)
+        .select();
 
-      setUser(data[0])
-      
+      setUser(data[0]);
 
       if (error) {
-        throw error
+        throw error;
       }
     } catch (error) {
       if (error instanceof Error) {
-        Alert.alert(error.message)
+        Alert.alert(error.message);
       }
     } finally {
-      setLoading(false)
-      
+      setLoading(false);
     }
   }
 
   return (
     <View style={styles.container}>
+      <View>
+        <Avatar
+          size={160}
+          url={avatarUrl}
+          onUpload={(url: string) => {
+            setAvatarUrl(url);
+            updateProfile({ username, website, avatar_url: url });
+          }}
+        />
+      </View>
       <View style={[styles.verticallySpaced, styles.mt20]}>
         <Input label="Email" value={session?.user?.email} disabled />
       </View>
       <View style={styles.verticallySpaced}>
-        <Input label="Username" value={username || ''} onChangeText={(text) => setUsername(text)} />
+        <Input
+          label="Username"
+          value={username || ""}
+          onChangeText={(text) => setUsername(text)}
+        />
       </View>
       <View style={styles.verticallySpaced}>
-        <Input label="Website" value={website || ''} onChangeText={(text) => setWebsite(text)} />
+        <Input
+          label="Website"
+          value={website || ""}
+          onChangeText={(text) => setWebsite(text)}
+        />
       </View>
 
+<View style={styles.buttonContainer}>
+
+      <View>
+        <Pressable
+          style={styles.button}
+          onPress={() =>
+            updateProfile({ username, website, avatar_url: avatarUrl })
+          }
+          disabled={loading}
+        >
+          <Text style={{ fontWeight: "bold", fontSize: 18, color: "#FFF" }}>
+            {loading ? "Loading..." : "Update"}
+          </Text>
+        </Pressable>
+      </View>
 
 
       <View>
-        <Avatar
-          size={200}
-          url={avatarUrl}
-          onUpload={(url: string) => {
-            setAvatarUrl(url)
-            updateProfile({ username, website, avatar_url: url })
+        <Pressable
+          style={styles.button2}
+          onPress={() => {
+            supabase.auth.signOut();
+            setUser(null);
+          }}
+        >
+          <Text style={{ fontWeight: "bold", fontSize: 18, color: "#FFF" }}>
+            Sign Out
+          </Text>
+        </Pressable>
+      </View>
+
+</View>
+
+      {/* <View style={styles.verticallySpaced}>
+        <Button
+          title="Sign Out"
+          onPress={() => {
+            supabase.auth.signOut();
+            setUser(null);
           }}
         />
-      </View>
-
-
-      <View style={[styles.verticallySpaced, styles.mt20]}>
-        <Button
-          title={loading ? 'Loading ...' : 'Update'}
-          onPress={() => updateProfile({ username, website, avatar_url: avatarUrl })}
-          disabled={loading}
-        />
-      </View>
-
-      <View style={styles.verticallySpaced}>
-        <Button title="Sign Out" onPress={() => {
-          console.log("inside onpress");
-          supabase.auth.signOut()
-          setUser(null)
-        }} />
-      </View>
+      </View> */}
     </View>
-
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     marginTop: 40,
     padding: 12,
+    display: "flex",
+    alignItems: "center",
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    display: "flex"
+
   },
   verticallySpaced: {
     paddingTop: 4,
     paddingBottom: 4,
-    alignSelf: 'stretch',
+    alignSelf: "stretch",
   },
   mt20: {
     marginTop: 20,
   },
-})
+  button: {
+    width: 120,
+    height: 38,
+    backgroundColor: "#3AD6A7",
+    borderRadius: 29,
+    justifyContent: "center",
+    alignItems: "center",
+    marginLeft: 30,
+    marginRight: 30,
+    marginBottom: 30
+  },
+  button2: {
+    width: 120,
+    height: 38,
+    backgroundColor: "#4C5B61",
+    borderRadius: 29,
+    justifyContent: "center",
+    alignItems: "center",
+    marginLeft: 30,
+    marginRight: 30,
+    marginBottom: 30
+  },
+  buttonText: {
+    color: "#FFF",
+  },
+});
