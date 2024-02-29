f8045901dc0ea073302532e2f12c0ae3ce468124
ChazzaT18 <charlie.tahsin123@gmail.com>
2024-02-29 03:40:34 +0000

    bug fix

--- component/RenderMenu.js
+++ component/RenderMenu.js
@@ -1,4 +1,4 @@
-import React from "react";
+// import React from "react";
 import { Text, View } from "react-native";
 import { Card } from "react-native-paper";
 import { getDishImageByUrl } from "../utils/getDishImageByUrl";
